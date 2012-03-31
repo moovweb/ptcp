b5c177106d36abd8331b8cf443ec0414bec65398
Zhigang Chen <zhigang.chen@moovweb.com>
2012-03-30 17:24:03 -0700

    updated var name

--- http_test.go
+++ http_test.go
@@ -16,17 +16,17 @@ func TestHttpClientServer(t *testing.T) {
 	logConfig := &log4go.LogConfig{ConsoleLogLevel: int(log4go.INFO)}
 	serverHandler := NewHttpServerHandler(logConfig, 4, "test_http_srv")
 	ListenAndServe(address, serverHandler, false)
-	upstreamHttpRequest := &UpstreamHttpRequest{}
-	upstreamHttpRequest.Request = ([]byte)("GET / HTTP/1.1\r\n\r\n")
+	uHttpRequest := &UpstreamHttpRequest{}
+	uHttpRequest.Request = ([]byte)("GET / HTTP/1.1\r\n\r\n")
 
-	buffer := bytes.NewBuffer(upstreamHttpRequest.Request)
+	buffer := bytes.NewBuffer(uHttpRequest.Request)
 	bf := bufio.NewReader(buffer)
 	httpRequest, err := http.ReadRequest(bf)
 	if err != nil {
 		t.Errorf("err: %v\n", err)
 		return
 	}
-	upstreamHttpRequest.HttpRequest = httpRequest
+	uHttpRequest.HttpRequest = httpRequest
 
 	for i := 0; i < 10; i++ {
 		connection, err := Connect(address)
@@ -37,7 +37,7 @@ func TestHttpClientServer(t *testing.T) {
 		wg.Add(1)
 		go func() {
 			defer connection.Close()
-			response, err := SendAndReceive(connection, clientHandler, upstreamHttpRequest)
+			response, err := SendAndReceive(connection, clientHandler, uHttpRequest)
 			if string(response.Bytes()) != DefaultOKResponse || err != nil {
 				t.Errorf("err: %v; received %q, expected %q\n", err, string(response.Bytes()), DefaultOKResponse)
 			}
@@ -54,15 +54,15 @@ func BenchmarkHttpClientServer(b *testing.B) {
 	logConfig := &log4go.LogConfig{ConsoleLogLevel: int(log4go.INFO)}
 	serverHandler := NewHttpServerHandler(logConfig, 1, "test_http_srv")
 	ListenAndServe(address, serverHandler, false)
-	upstreamHttpRequest := &UpstreamHttpRequest{}
-	upstreamHttpRequest.Request = ([]byte)("GET / HTTP/1.1\r\nConnection: keep-alive\r\n\r\n")
-	buffer := bytes.NewBuffer(upstreamHttpRequest.Request)
+	uHttpRequest := &UpstreamHttpRequest{}
+	uHttpRequest.Request = ([]byte)("GET / HTTP/1.1\r\nConnection: keep-alive\r\n\r\n")
+	buffer := bytes.NewBuffer(uHttpRequest.Request)
 	bf := bufio.NewReader(buffer)
 	httpRequest, err := http.ReadRequest(bf)
 	if err != nil {
 		log.Fatalf("err: %v\n", err)
 	}
-	upstreamHttpRequest.HttpRequest = httpRequest
+	uHttpRequest.HttpRequest = httpRequest
 
 	connection, err := Connect(address)
 	connection.EnableSaveReadData()
@@ -73,7 +73,7 @@ func BenchmarkHttpClientServer(b *testing.B) {
 	defer connection.Close()
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		_, err := SendAndReceive(connection, clientHandler, upstreamHttpRequest)
+		_, err := SendAndReceive(connection, clientHandler, uHttpRequest)
 		if err != nil {
 			log.Fatalf("err: %s", err.String())
 		}
